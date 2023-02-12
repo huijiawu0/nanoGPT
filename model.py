b1349932a8241f6d9b6315370c485916c95b7800
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-02-12 17:30:53 +0800

    add chs ds

--- model.py
+++ model.py
@@ -237,9 +237,10 @@ class GPT(nn.Module):
             'gpt2-large': dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
             'gpt2-xl': dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
             'IDEA-CCNL/Wenzhong-GPT2-110M': dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-            'IDEA-CCNL/Wenzhong2.0-GPT2-3.5B-chinese': dict(n_layer=30, n_head=32, n_embd=3072, block_size=12288, )
+            'IDEA-CCNL/Wenzhong2.0-GPT2-3.5B-chinese':
+                dict(n_layer=30, n_head=32, n_embd=3072, block_size=12288, vocab_size=50304)
         }[model_type]
-        if not model_type == 'IDEA-CCNL/Wenzhong-GPT2-110M':
+        if model_type.startswith('gpt2'):
             print("forcing vocab_size=50257, block_size=1024, bias=True")
             config_args['vocab_size'] = 50257  # always 50257 for GPT model checkpoints
             config_args['block_size'] = 1024  # always 1024 for GPT model checkpoints
