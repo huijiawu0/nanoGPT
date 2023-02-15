f7e5609972cef7b30a1d32d6b6bbe2697af5a963
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-02-15 09:57:51 +0800

    fix bug

--- s3.py
+++ s3.py
@@ -12,13 +12,13 @@ model.to("cuda")
 def get_single(question):
     inputs = tokenizer(question, return_tensors='pt').to("cuda")
     assert isinstance(model, GPT2LMHeadModel)
+    stop_tok = tokenizer.encode('。')[0]
     generation_output = model.generate(**inputs,
-                                       do_sample=True,
-                                       top_p=0.9,
+                                       do_sample=False,
                                        early_stopping=True,
-                                       num_beams=1,
-                                       max_length=300,
-                                       no_repeat_ngram_size=2,
+                                       max_length=500,
+                                       eos_token_id=stop_tok,
+                                       pad_token_id=0,
                                        num_return_sequences=1
                                        )
     ret = []
@@ -31,9 +31,10 @@ out = sys.argv[2]
 with open("qa_pair.txt", 'r') as f, open(out, 'w') as g:
     for idx, line in enumerate(f):
         r = line.strip().split('|||')
-        if len(r) != 2: continue
+        if len(r) != 2:
+            continue
         q, a = r
         res = get_single(q)
         g.write("%s|||%s|||%s\n" % (q, a, '<s>'.join(res)))
-        if idx > 10:
+        if idx > 100:
             break
