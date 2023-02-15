8eba62bcd25402fd8e1f3d4cc6ec02c994f43dc8
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-02-15 09:43:05 +0800

    add files

--- s3.py
+++ s3.py
@@ -7,32 +7,27 @@ config = GPT2Config.from_pretrained(hf_model_path)
 model = GPT2LMHeadModel.from_pretrained(hf_model_path)
 
 model.to("cuda")
-max_length = int(sys.argv[2])
-num_return_sequences = int(sys.argv[3])
 
 
 def get_single(question):
-    # question = "写一篇神经网络的报告"
     inputs = tokenizer(question, return_tensors='pt').to("cuda")
     assert isinstance(model, GPT2LMHeadModel)
     generation_output = model.generate(**inputs,
-                                       return_dict_in_generate=True,
-                                       output_scores=True,
-                                       max_length=max_length,
                                        do_sample=True,
-                                       top_p=0.6,
-                                       # num_beams=5,
-                                       eos_token_id=50256,
-                                       pad_token_id=0,
-                                       num_return_sequences=num_return_sequences,
+                                       top_p=0.9,
+                                       early_stopping=True,
+                                       num_beams=1,
+                                       max_length=300,
+                                       no_repeat_ngram_size=2,
+                                       num_return_sequences=1
                                        )
     ret = []
-    for idx, sentence in enumerate(generation_output.sequences):
+    for idx, sentence in enumerate(generation_output):
         s = tokenizer.decode(sentence).split('<|endoftext|>')[0]
         ret.append(s)
     return ret
 
-out = sys.argv[4]
+out = sys.argv[2]
 with open("qa_pair.txt", 'r') as f, open(out, 'w') as g:
     for idx, line in enumerate(f):
         r = line.strip().split('|||')
