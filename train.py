c572c19bc325d904b82bbf1c93d8617a688c383a
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-02-24 22:23:36 +0800

    add s3

--- train.py
+++ train.py
@@ -202,8 +202,7 @@ if init_from == 'resume':
 # compile the model
 if compile:
     print("compiling the model... (takes a ~minute)")
-    unoptimized_model = model
-    model = torch.compile(model)  # requires PyTorch 2.0
+    model = torch.compile(model)
 
 # wrap model into DDP container
 if ddp:
