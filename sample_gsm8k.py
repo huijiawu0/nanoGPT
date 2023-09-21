99588f95833028c0ce0dda1672c626a4acf1ec66
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-09-21 15:53:28 +0800

    add gsm8k data

---
+++ sample_gsm8k.py
@@ -0,0 +1,145 @@
+"""
+Sample from a trained model
+"""
+import json
+import os
+import pickle
+import random
+import re
+from contextlib import nullcontext
+
+import tiktoken
+import torch
+from tqdm import tqdm
+
+from model import GPTConfig, GPT
+
+# -----------------------------------------------------------------------------
+init_from = 'resume'  # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
+out_dir = 'out'  # ignored if init_from is not 'resume'
+start = "\n"  # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+num_samples = 10  # number of samples to draw
+max_new_tokens = 500  # number of tokens generated in each sample
+temperature = 0.8  # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+top_k = 200  # retain only the top_k most likely tokens, clamp others to have 0 probability
+seed = 1337
+device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
+compile = False # use PyTorch 2.0 to compile the model to be faster
+exec(open('configurator.py').read()) # overrides from command line or config file
+# -----------------------------------------------------------------------------
+
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
+device_type = 'cuda' if 'cuda' in device else 'cpu'  # for later use in torch.autocast
+ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+
+# model
+if init_from == 'resume':
+    # init from a model saved in a specific directory
+    ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+    checkpoint = torch.load(ckpt_path, map_location=device)
+    gptconf = GPTConfig(**checkpoint['model_args'])
+    model = GPT(gptconf)
+    state_dict = checkpoint['model']
+    unwanted_prefix = '_orig_mod.'
+    for k, v in list(state_dict.items()):
+        if k.startswith(unwanted_prefix):
+            state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+    model.load_state_dict(state_dict)
+elif init_from.startswith('gpt2'):
+    # init from a given GPT-2 model
+    model = GPT.from_pretrained(init_from, dict(dropout=0.0))
+
+model.eval()
+model.to(device)
+if compile:
+    model = torch.compile(model)  # requires PyTorch 2.0 (optional)
+
+# look for the meta pickle in case it is available in the dataset folder
+load_meta = False
+if init_from == 'resume' and 'config' in checkpoint and 'dataset' in checkpoint[
+    'config']:  # older checkpoints might not have these...
+    meta_path = os.path.join('data', checkpoint['config']['dataset'], 'meta.pkl')
+    load_meta = os.path.exists(meta_path)
+if load_meta:
+    print(f"Loading meta from {meta_path}...")
+    with open(meta_path, 'rb') as f:
+        meta = pickle.load(f)
+    # TODO want to make this more general to arbitrary encoder/decoder schemes
+    stoi, itos = meta['stoi'], meta['itos']
+    encode = lambda s: [stoi[c] for c in s]
+    decode = lambda l: ''.join([itos[i] for i in l])
+else:
+    # ok let's assume gpt-2 encodings by default
+    print("No meta.pkl found, assuming GPT-2 encodings...")
+    enc = tiktoken.get_encoding("gpt2")
+    encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
+    decode = lambda l: enc.decode(l)
+
+
+def read_jsonl(path: str):
+    with open(path) as fh:
+        return [json.loads(line) for line in fh.readlines() if line]
+
+
+def get_examples(split):
+    path = os.path.join("data/", f"{split}.jsonl")
+    examples = read_jsonl(path)
+
+    for ex in examples:
+        ex.update(question=ex["question"] + "\n")
+        ex.update(answer=ex["answer"] + "<|endoftext|>")
+
+    print(f"{len(examples)} {split} examples")
+    return examples
+
+
+def extract_answer(completion):
+    if completion.find('\u0000') >= 0:
+        completion = completion[0:completion.find('\u0000')]
+    match = ANS_RE.search(completion)
+    if match:
+        match_str = match.group(1).strip()
+        match_str = match_str.replace(",", "")
+        try:
+            float(match_str)
+        except BaseException:
+            return INVALID_ANS
+        return match_str
+    else:
+        return INVALID_ANS
+
+test_examples = get_examples("test")
+random_example = random.choice(test_examples)
+sample_len = 100
+ANS_RE = re.compile(r"#### (\-?[0-9\.\,]+)")
+INVALID_ANS = "[invalid]"
+
+pred_ans = []
+gold_ans = []
+with torch.no_grad():
+    with ctx:
+        for example in tqdm(test_examples):
+            qn = example["question"]
+            x = (torch.tensor(encode(qn), dtype=torch.long, device=device)[None, ...])
+            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+            s = decode(y[0].tolist())
+            ans_ext = extract_answer(s)
+            pred_ans.append(ans_ext)
+            gold_ans.append(extract_answer(example["answer"]))
+            print(''.join(s.split()).split('<|endoftext|>')[0])
+            print('---------------')
+
+print(pred_ans)
+print(gold_ans)
+
+cor = 0
+rg = range(min(len(pred_ans), len(gold_ans)))
+for i in rg:
+    if pred_ans[i] != INVALID_ANS and abs(float(pred_ans[i]) - float(gold_ans[i])) < 1e-4:
+        cor += 1
+print(cor, cor / len(list(rg)) * 100, len(rg))
